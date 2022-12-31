1da61f84c415ab6c8f5de21e928dfa1f2674f76f
gustavobatistadev <gustavobatistadev@gmail.com>
2022-12-31 18:09:28 -0300

    herdando base.html

---
+++ recipes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'recipes/pages/home.html')
+
+def recipe(request: HttpRequest, id: int) -> HttpResponse:
+    return render(request, 'recipes/pages/recipe-view.html')
